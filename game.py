a3b819a21500abe9be69572c25efbbafbec7b5c2
Evan Owens <eowens9516@floridapoly.edu>
2025-11-12 16:49:21 +0000

    Computer

--- game.py
+++ game.py
@@ -1,1 +1,9 @@
+import random
+
 print("Welcome to Rock-Paper-Scissors!")
+
+game = ["rock", "paper", "scissors"]
+
+random_num = random.randint(0, 2)
+
+print(f"Computer chose: {game[random_num]}")
